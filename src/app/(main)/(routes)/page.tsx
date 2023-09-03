7eb6dfcd3846655350e1fb5df841af4b3bee377a
SaiSawant1 <saisawant2003@gmail.com>
2023-09-03 10:53:04 +0530

    added authentication with the help of clerk of clerk library

---
+++ src/app/(main)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+const HomePage = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+export default HomePage;
