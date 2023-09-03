db5ac17a4dee7718030a1912f20ea8e4a1c5911f
SaiSawant1 <saisawant2003@gmail.com>
2023-09-03 11:15:53 +0530

    added theme switcher

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 const HomePage = () => {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle/>
     </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
@@ -17,8 +19,17 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={openSans.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(openSans.className,"bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="chat-mates-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
