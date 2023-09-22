817b217816a2b7239d6d2bf193ab4d4628a56c33
SaiSawant1 <saisawant2003@gmail.com>
2023-09-22 21:36:57 +0530

    socket live chat comepletd

--- src/components/chat/chat-item.tsx
+++ src/components/chat/chat-item.tsx
@@ -16,6 +16,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useModal } from "@/hooks/use-modal-store";
+import { useRouter, useParams } from "next/navigation";
 
 interface ChatItemProps {
   id: string;
@@ -59,7 +60,8 @@ const ChatItem = ({
   const [isEditing, setIsEditing] = React.useState(false);
   const { onOpen } = useModal();
   const fileType = fileUrl?.split(".").pop();
-
+  const router = useRouter();
+  const params = useParams();
   const isAdmin = currentMember.role === MemberRole.ADMIN;
   const isModerator = currentMember.role === MemberRole.MODERATOR;
   const isOwner = currentMember.id === member.id;
@@ -106,16 +108,29 @@ const ChatItem = ({
     return () => window.removeEventListener("keydown", handleKey);
   }, []);
 
+  const onMemberClicked = () => {
+    if (member.id === currentMember.id) {
+      return;
+    }
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  };
+
   return (
     <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
       <div className="group flex gap-x-2 items-start w-full">
-        <div className="cursor-pointer hover:drop-shadow-md transition-all">
+        <div
+          onClick={onMemberClicked}
+          className="cursor-pointer hover:drop-shadow-md transition-all"
+        >
           <UserAvatar src={member.profile.imageUrl} />
         </div>
         <div className="flex flex-col w-full">
           <div className="flex items-center gap-x-2 ">
             <div className="flex items-center">
-              <p className="font-semibold text-sm hover:underline cursor-pointer">
+              <p
+                onClick={onMemberClicked}
+                className="font-semibold text-sm hover:underline cursor-pointer"
+              >
                 {member.profile.name}
               </p>
               <ActionToolTip label={member.role}>
--- src/components/chat/chat-messages.tsx
+++ src/components/chat/chat-messages.tsx
@@ -1,11 +1,12 @@
 "use client";
-import {format} from "date-fns"
+import { format } from "date-fns";
 import { Member, Message, Profile } from "@prisma/client";
 import ChatWelcome from "@/components/chat/chat-welcome";
 import useChatQuery from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import ChatItem from "@/components/chat/chat-item";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 type MessageWithMemberWithProfile = Message & {
   member: Member & {
@@ -13,7 +14,7 @@ type MessageWithMemberWithProfile = Message & {
   };
 };
 
-const DATE_FORMAT="d MMM yyyy, HH:mm";
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 interface ChatMessagesProps {
   name: string;
@@ -39,6 +40,8 @@ const ChatMessages: React.FC<ChatMessagesProps> = ({
   socketQuery,
 }) => {
   const queryKey = `chat:${chatId}`;
+  const addKey = `chat:${chatId}:messages`;
+  const updateKey = `chat:${chatId}:messages:update`;
   const { status, data, fetchNextPage, hasNextPage, isFetchingNextPage } =
     useChatQuery({
       apiUrl,
@@ -46,6 +49,8 @@ const ChatMessages: React.FC<ChatMessagesProps> = ({
       paramValue,
       queryKey,
     });
+
+  useChatSocket({ queryKey, addKey, updateKey });
 
   if (status === "loading") {
     return (
@@ -72,23 +77,22 @@ const ChatMessages: React.FC<ChatMessagesProps> = ({
     <div className="flex-1 flex flex-col py-4 overflow-y-auto">
       <div className="flex-1" />
       <ChatWelcome type={type} name={name} />
-      <div className=" flex-col-reverse mt-auto ">
+      <div className=" flex-col-reverse flex mt-auto ">
         {data?.pages.map((group, i) => (
           <Fragment key={i}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <ChatItem 
-              currentMember={member}
-              member={message.member}
-              key={message.id}
-              id={message.id}
-              content={message.content}
-              fileUrl={message.fileUrl}
-              deleted={message.delete}
-              timestamp={format(new Date(message.createdAt),DATE_FORMAT)}
-              isUpdated={message.updatedAt!==message.createdAt}
-              socketQuery={socketQuery}
-              socketUrl={socketUrl}
-
+              <ChatItem
+                currentMember={member}
+                member={message.member}
+                key={message.id}
+                id={message.id}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.delete}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketQuery={socketQuery}
+                socketUrl={socketUrl}
               />
             ))}
           </Fragment>
